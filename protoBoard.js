2656a9baeaa6be46bc186563ad510caff7fe2de1
ChristopherGoldsmith <59451779+ChristopherGoldsmith@users.noreply.github.com>
2020-05-21 17:40:55 -0700

    Add files via upload

--- protoBoard.js
+++ protoBoard.js
@@ -13,8 +13,12 @@ let hand = [];
 let finalR = [];
 let score = 0;
 let winHand = false;
-
-//randomizer
+//
+let gloop = function(){
+    return document.createElement()
+};
+//
+//randomizer and math functions
 var shuffle = function (array) {
 	var currentIndex = array.length;
 	var temporaryValue, randomIndex;
@@ -27,20 +31,52 @@ var shuffle = function (array) {
 	}
 	return array;
 };
-
+function compSort(arr){
+    for (y = 0; y < arr.length; y++){
+        for(z = 0; z < arr.length; z++){
+            if(arr[y] == arr[z] && y != z){
+                console.log('failure of test')
+                return false;
+            } else{ console.log('success!')}
+        };
+    };
+};
+function royalty(arr){
+    for (y = 0; y < arr.length; y++){
+        if(arr[0] + y != 10 + y){
+            console.log('not royal')
+                return false;
+    } else{
+            return console.log('Royal!')
+        }
+    };
+}
 // Deck Generatiom Code
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+let seven = 7;
+let eight = 8;
+let nine = 9;
+let ten = 10;
+let jack = 11;
+let queen = 12;
+let king = 13;
+let ace = 14;
 let suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];
-let value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+let value = [two, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace];
 let deck52 = [];
 
 //deck generting functions
 function deckGen(suit, value){
     this.name = `The ${value} of ${suit}`
     this.suit = suit,
-    this.value = value
-    this.matchV = [];
-    this.matchS = [];
-}
+    this.value = value,
+    this.matchV = [],
+    this.matchS = []
+};
 
 function makeDeck(){
     for(i = 0; i < suits.length; i++){
@@ -61,13 +97,17 @@ function clearAndSet(){
 function draw(){
     i = 0;
     n = 5
-    while(n > i){
-        hand.push(deck52[0]);
-        deck52.shift();
-        console.log(hand, deck52);
-        i = hand.length;
+    if (hand.length < 5){
+        while(n > i){
+            hand.push(deck52[0]);
+            deck52.shift();
+            //console.log(hand, deck52);
+            i = hand.length;
      
-    };
+        };
+    } else{
+        return console.log('Hand Full!')
+    }
     handPop();
     return hand, deck52;
 };
@@ -89,15 +129,7 @@ function delCard(n, m){
 function scoreUp(s){
     score += s;
     document.getElementById('score').innerHTML = `${score}`
-}
-/*function checkValue(arr, num){
-	let len = arr.length;
-	for(i = len; i > 0; i--){
-  	if(arr[i] <= num){
-    	arr.splice(i, 1);
-    }
-  }
-};*/
+};
 function matchingPlus(arr, matched){
     return arr.matchV.push(matched);
 };
@@ -172,7 +204,7 @@ function checkValSame(arr){
         };
      };
      if(testy.length == 5){
-         scoreUp(750);
+         scoreUp(1250);
          return console.log(`Full House! ${score}`);
      } else {
          console.log('conditions not met!')
@@ -187,7 +219,7 @@ function checkValSame(arr){
         };
      };
      if(testy.length == 4){
-         scoreUp(2000);
+         scoreUp(2500);
          return console.log(`Four of a Kind ${score}`);
      } else {
          console.log('conditions not met!')
@@ -202,23 +234,63 @@ function checkValSame(arr){
         };
      };
      if(testy.length == 5){
-         scoreUp(1000);
+         scoreUp(500);
          return console.log(`Flush! ${score}`);
      } else {
          console.log('conditions not met!')
      };
  };
+ function straight(){
+     let testy = [];
+     let testyStr = [];
+     let high = 0;
+     let low = 0;
+     for(i = 0; i < hand.length; i++){
+         testy.push(hand[i].value);
+     }
+     high = Math.max.apply(null, testy);
+     low = Math.min.apply(null, testy);
+     for(i = 0; i < hand.length; i++){
+        if(high == low + 4){
+            testyStr.push(hand[i].value);
+            console.log(testyStr.sort());
+        }
+     };
+     testy = [];
+     for(i = 0; i < hand.length; i++){
+        findS = hand[i].matchS.length;
+        if(findS == 5){
+            testy.push(findS);
+        };
+     };
+     winStr = compSort(testyStr);
+     royal = royalty(testyStr);
+     if(winStr){
+         console.log('failure or error winStr')
+         return false;
+     }
+     if(testyStr.length == 5 && testy.length == 5 && !royal){
+        scoreUp(50000) 
+        return console.log('ROYAL FLUSH!')
+     }else if (testyStr.length == 5 && testy.length == 5){
+        scoreUp(10000);
+        return console.log('Straight Flush!')
+     } else if (testyStr.length == 5 && testy.length != 5){
+         scoreUp(2000);
+         return console.log('Straight!')
+     } else{
+         return false;
+     }
+     
+ }
  function calcHand(){
+    straight();
     fullHouse();
     fourofKind();
     flush();
     twoofKind();
     threeofKind();
  };
-
-const straight = [1000];// no objects with a matching value, but a straight sequence
-const straightFlush = [7500];// both conditions for a straight and a flush
-const royalFlush = [25000];// the conditions for a straightFlush but with 10, J, Q, K, A
 
 
 //startup functions
---
+++ winCon.js
@@ -0,0 +1,3 @@
+function calcHand(n){
+    
+}
